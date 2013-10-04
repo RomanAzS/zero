6c873562031ace896e72fb1a94a90065901d0873
Aziz S <azi.r3d@gmail.com>
2013-10-05 01:13:03 +0200

    QUIT, PART/KICK, and something else probably added

--- handler2.py
+++ handler2.py
@@ -1,26 +1,27 @@
 import time
 
 channels = {}
-just_joined = ''
 
 def user_join(message):
     print(message)
     channel = message[2][:-1]
     print(channel)
-    print(message[1].split('!')[0])
-    if message[1].split('!')[0] == 'cero':
+    user = message[1].split('!')[0]
+    if user == 'cero':
         channels[channel] = {}
         print(channels)
         just_joined = channel
         print("Joined channel " + channel)
         time.sleep(5)
         return "WHO %s\r\n" % channel
+    else:
+        return "WHO %s\r\n" % user
 
 def invite(message):
     channel = message[2]
     return "JOIN %s\r\n" % channel #WHO %s" % (channel, channel)
 
-def who(message):
+def who_reply(message):
 #    print(message)
     print(channels)
     msg = message[1].split()
@@ -40,11 +41,35 @@ def who(message):
 def user_part(message):
     pass
 
+def user_gone(message): # this is assuming KICK an PART follow same format
+    """User parted/kicked"""
+    channel = message[2][:-1]
+    user = message[1].split('!')[0]
+    if user != 'cero':
+        del channel[channel][user]
+    else: del channel[channel]    
+    pass
+
+def user_quat(message):
+    # checked; server kills/klines are also QUITs 
+    channel = message[2][:-1]
+    user = message[1].split('!')[0]
+    if user != 'cero':
+        del channel[channel][user]
+    pass
+
+def user_nick(message):
+    """When a user changes their nick remap their entry in chan dict
+    to their new nick"""
+    pass
+
+handledTypes = {'JOIN': user_join, 'PART': user_part, 'INVITE': invite,
+    '352': who_reply, 'QUIT': user_quat, 'PART': user_gone, 'KICK': user_gone}
+
 def handler(raw_message):
     message = raw_message.split(':')
     try:
         messageType = message[1].split()[1].strip()
-        handledTypes = {'JOIN': user_join, 'PART': user_part, 'INVITE': invite, '352': who}
         if messageType in list(handledTypes.keys()):
             return handledTypes[messageType](message)
         else: return
