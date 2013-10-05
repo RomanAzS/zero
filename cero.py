ec47d08029068a078acb9c592f53c2308ee1aebb
Aziz S <azi.r3d@gmail.com>
2013-10-05 11:30:32 +0200

    Added command line options for running the bot; host, port, pass, nickserv pass, and nick can all be specified from the command line

--- cero.py
+++ cero.py
@@ -1,11 +1,26 @@
 import socket
 import handler2
+import sys
+import argparse
 
-HOST = "irc.awfulnet.org"
+#HOST = "irc.awfulnet.org"
 PORT = 6667
 NICK = "cero"
 IDENT = "zero"
 REALNAME = "1.5"
+
+# handles args from command line
+parser = argparse.ArgumentParser()
+parser.add_argument("hostname", help="server hostname to connect to", type=str)
+parser.add_argument("-p", "--port", help="specify port number to use",type=int)
+parser.add_argument("-w", "--pass", help="server password for authentication", type=str)
+parser.add_argument("-i", "--identify", help="password for NickServ identification", type=str)
+parser.add_argument("-n", "--nick", help="use a different nick than default", type=str)
+args = parser.parse_args()
+HOST = args.hostname
+if args.port != None: PORT = args.port
+if args.nick != None: NICK = args.nick
+print("Connecting to {0} {1}..." .format(HOST, PORT))
 
 s = socket.socket()
 s.connect((HOST, PORT)) # connect to host and port
@@ -24,5 +39,5 @@ while loop == 0:
         stuff = handler2.handler(line)
         if stuff != None: 
             s.send(stuff.encode())
-            if stuff.startswith("QUIT"): loop = 6
+            if line.startswith("ERROR"): loop = 6
     
